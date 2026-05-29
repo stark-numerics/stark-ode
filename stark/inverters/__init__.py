5f80a152c7e6d21cbe4313b01fafa10435d53ce0
Jon M Fellows <jon.m.fellows@gmail.com>
2026-05-29 11:43:25 +0100

    Moved existing inverters under inverters/legacy ahead of refactor

--- stark/inverters/__init__.py
+++ stark/inverters/__init__.py
@@ -1,8 +1,8 @@
 """Built-in linear inverse-action operators."""
 
-from stark.inverters.bicgstab import InverterBiCGStab
-from stark.inverters.fgmres import InverterFGMRES
-from stark.inverters.gmres import InverterGMRES
+from stark.inverters.legacy.bicgstab import InverterBiCGStab
+from stark.inverters.legacy.fgmres import InverterFGMRES
+from stark.inverters.legacy.gmres import InverterGMRES
 from stark.inverters.support.descriptor import InverterDescriptor
 from stark.inverters.support.policy import InverterPolicy
 from stark.inverters.support.tolerance import InverterTolerance
